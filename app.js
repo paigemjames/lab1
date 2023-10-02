a61d362c302ff05f0a67b9634de84cdbd00980b0
paigemjames <paigemjames98@gmail.com>
2023-10-02 19:14:31 -0400

    save for dinner

--- app.js
+++ app.js
@@ -13,16 +13,26 @@ app.use('/css', express.static(__dirname + 'public/images'))
 
 // Set Templating Engine
 app.use(expressLayouts)
+app.set('layout', './layouts/basic-layout')
 app.set('view engine', 'ejs')
 
 // Routes
 app.get('', (req, res) => {
     res.render('index', { title: 'Home Page'})
 })
-
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About Page', layout: './layouts/sidebar' })
+    res.render('about', { title: 'About Page'})
 })
+app.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects Page'})
+})
+app.get('/services', (req, res) => {
+    res.render('services', { title: 'Services Page'})
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact Page'})
+})
+
 
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`))
