efe4f1fa3d43f54d7ffdd11b43b9f676d5aa3881
paigemjames <paigemjames98@gmail.com>
2023-10-02 18:41:29 -0400

    this is working again!

---
+++ app.js
@@ -0,0 +1,28 @@
+// Imports
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+const port = 5000
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/css', express.static(__dirname + 'public/js'))
+app.use('/css', express.static(__dirname + 'public/images'))
+
+// Set Templating Engine
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+// Routes
+app.get('', (req, res) => {
+    res.render('index', { title: 'Home Page'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Page', layout: './layouts/sidebar' })
+})
+
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
